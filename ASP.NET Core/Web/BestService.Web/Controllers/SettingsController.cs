47cb223f219c9e1a68acc05348ce430052f07934
vladosfi <sfi@abv.bg>
2020-03-01 20:58:33 +0200

    Initial Commit

---
+++ ASP.NET Core/Web/BestService.Web/Controllers/SettingsController.cs
@@ -0,0 +1,43 @@
+﻿namespace BestService.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using BestService.Data.Common.Repositories;
+    using BestService.Data.Models;
+    using BestService.Services.Data;
+    using BestService.Web.ViewModels.Settings;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SettingsController : BaseController
+    {
+        private readonly ISettingsService settingsService;
+
+        private readonly IDeletableEntityRepository<Setting> repository;
+
+        public SettingsController(ISettingsService settingsService, IDeletableEntityRepository<Setting> repository)
+        {
+            this.settingsService = settingsService;
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var settings = this.settingsService.GetAll<SettingViewModel>();
+            var model = new SettingsListViewModel { Settings = settings };
+            return this.View(model);
+        }
+
+        public async Task<IActionResult> InsertSetting()
+        {
+            var random = new Random();
+            var setting = new Setting { Name = $"Name_{random.Next()}", Value = $"Value_{random.Next()}" };
+
+            await this.repository.AddAsync(setting);
+            await this.repository.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
