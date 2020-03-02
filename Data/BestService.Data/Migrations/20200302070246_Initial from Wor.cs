dbc6ac5fe01c3c833b3541df2a3bb3f1aae0e2ca
vladosfi <vladosfi@gmail.com>
2020-03-02 11:00:35 +0200

    Footer fix

---
+++ Data/BestService.Data/Migrations/20200302070246_Initial from Wor.cs
@@ -0,0 +1,15 @@
+﻿namespace BestService.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class InitialfromWor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
