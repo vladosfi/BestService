135cf5a17707165b53790a084e07e2cceca6b5c8
vladosfi <sfi@abv.bg>
2020-03-03 16:37:14 +0200

    Update theme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BestService-Web-Project
 Best Sercvice - ASP.NET CORE 3.1
+- Partial View: _Navigation
---
+++ Web/BestService.Web/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿namespace BestService.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
