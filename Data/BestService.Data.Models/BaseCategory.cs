f81c9b65bec2d7687693913cf60a6b9751d93d83
vladosfi <vladosfi@gmail.com>
2020-03-13 16:34:18 +0200

    Change base category

--- Data/BestService.Data.Models/BaseCategory.cs
+++ Data/BestService.Data.Models/BaseCategory.cs
@@ -15,6 +15,6 @@
 
         public string Description { get; set; }
 
-        public IEnumerable<Category> Categories { get; set; }
+        public virtual IEnumerable<Category> Categories { get; set; }
     }
 }
