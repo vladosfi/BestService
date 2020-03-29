387743e4613450a9eab5a3d0c785ab929a32dc54
vladosfi <sfi@abv.bg>
2020-03-29 20:56:53 +0300

    Update seedings

--- Data/BestService.Data/Seeding/CategoriesSeeder.cs
+++ Data/BestService.Data/Seeding/CategoriesSeeder.cs
@@ -18,16 +18,20 @@
 
             var categories = new Dictionary<string, string>
             {
-                { "Advertising", "Advertising is a marketing communication that employs an openly sponsored, non-personal message to promote or sell a product, service or idea." },
-                { "IT", "The information technology (IT) is comprised of companies that produce software, hardware or semiconductor equipment, or companies that provide internet or related services." },
-                { "Entertainment", "Entertainment is involved in providing entertainment: radio and television and films and theater" },
-                { "Agriculture", "Agriculture is the sector of the economy that produces livestock, poultry, fish and crops." },
-                { "Infrastructure", "Infrastructure are basic facilities, structures, equipment, technologies and services that act as the foundation for economic activity and quality of life" },
-                { "Construction", "Construction is the sector of the economy that builds, improves and repairs buildings, structures, infrastructure and land features. The following are common components of the construction industry." },
-                { "Healthcare",  "Healthcare sector consists of businesses that provide medical services, manufacture medical equipment or drugs, provide medical insurance, or otherwise facilitate the provision of healthcare to patients." },
-                { "Education", "Educational Services is composed of establishments that provide instruction and training on a wide variety of subjects." },
-                { "Transport", "The transportation sector is a category of companies that provide services to move people or goods, as well as transportation infrastructure. " },
-                { "Fashion", "Fashion is a large industry that includes elements of design, manufacturing, marketing, supply chain, retailing, ecommerce and media. Products include apparel, footwear, accessories, cosmetics and materials such as textiles." },
+                { "IT","The information technology (IT) is comprised of companies that produce software, hardware or semiconductor equipment, or companies that provide internet or related services." },
+                { "Healthcare","Businesses that provide medical services, manufacture medical equipment or drugs, provide medical insurance, or otherwise facilitate the provision of healthcare to patients." },
+                { "Transport","Companies that provide services to move people or goods, as well as transportation infrastructure." },
+                { "Other sectors","Anyting else not included in other categories" },
+                { "Entertainment","Providing entertainment: radio and television and films and theater" },
+                { "Agriculture","Produces livestock, poultry, fish and crops." },
+                { "Art","Decorative applied arts objects used to enhance daily life and the interiors of homes. Such objects include items of clothing, fabrics for clothing and upholstery" },
+                { "Education","Establishments that provide instruction and training on a wide variety of subjects." },
+                { "Sport","Market in which people, activities, business, and organizations are involved in producing, facilitating, promoting, or organizing any activity, experience, or business enterprise focused on sports." },
+                { "Construction","The branch of manufacture and trade based on the building, maintaining, and repairing structures." },
+                { "Tourism","All about providing necessary means to assist tourists throughout their travelling." },
+                { "Trade","Involves the transfer of goods or services from one person or entity to another, often in exchange for money." },
+                { "Services","Part of the economy that creates services rather than tangible objects." },
+                { "Properties","Buying and selling of properties used as homes or for non-professional purposes." },
             };
 
             foreach (var category in categories)
