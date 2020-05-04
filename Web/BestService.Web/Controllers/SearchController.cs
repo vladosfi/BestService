edb9871583346efdde93ae15c4cd0368f2d8dddd
vladosfi <vladosfi@gmail.com>
2020-05-04 17:25:22 +0300

    Working On Search

--- Web/BestService.Web/Controllers/SearchController.cs
+++ Web/BestService.Web/Controllers/SearchController.cs
@@ -5,6 +5,8 @@
     using System.Threading.Tasks;
 
     using BestService.Data;
+    using BestService.Services.Data;
+    using BestService.Web.ViewModels.Companies;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
 
@@ -13,27 +15,29 @@
     public class SearchController : ControllerBase
     {
         private readonly ApplicationDbContext dbContext;
+        private readonly ICompaniesService companiesService;
 
-        public SearchController(ApplicationDbContext dbContext)
+        public SearchController(ApplicationDbContext dbContext, ICompaniesService companiesService)
         {
             this.dbContext = dbContext;
+            this.companiesService = companiesService;
         }
 
         [Produces("application/json")]
-        [HttpGet("search")]
-        public async Task<IActionResult> Search()
+        [HttpGet("autocomplete")]
+        public async Task<IActionResult> Autocomplete()
         {
             try
             {
                 string term = this.HttpContext.Request.Query["term"].ToString();
                 var companyName = await this.dbContext.Companies.Where(x => x.Name.Contains(term))
+                    .OrderBy(x => x.Name)
                     .Select(x => x.Name).ToListAsync();
 
                 return this.Ok(companyName);
             }
             catch (Exception)
             {
-
                 return this.BadRequest();
             }
         }
